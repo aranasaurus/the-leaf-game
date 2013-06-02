a874f97fa45c4cec386ae5817e6b961a25311dc9
Ryan Arana <rarana@esri.com>
2013-06-02 10:43:13 -0400

    Add graphics for the leaves and improve animation

--- src/game.js
+++ src/game.js
@@ -4,16 +4,12 @@ var Game = {
   start: function() {
     Crafty.init(550, 440);
   
-    /*
-  	Crafty.sprite(64, "images/fruit.png", {
-  		banana: [0,0],
-  		apple: [1,0],
-  		watermelon: [2,0],
-  		orange: [3,0],
-  		coconut: [4,0],
-  		lemon: [5,0]
+  	Crafty.sprite(128, "assets/128xleaves.png", {
+  		spr_leaf0: [0,0],
+  		spr_leaf1: [0,1],
+  		spr_leaf2: [0,2],
+  		spr_leaf3: [0,3]
   	});
-    */
   
     Crafty.background("#1B8EE0");
   
@@ -21,33 +17,48 @@ var Game = {
   		score = 0;
   
   	Crafty.c("Leaf", {
-  		_colors: ["#fff", "#f00", "#0f0", "#f99", "#00f", "#9f9"],
   		_xspeed: 0,
   		_yspeed: 0,
   
   		init: function() {
   			var index = Crafty.math.randomInt(0, 5);
   
-        this.addComponent("2D, Color, Mouse");
-        this.w = 48;
-        this.h = 48;
-        this.color(this._colors[index]);
+        this.addComponent("spr_leaf"+index);
+        this.w = 64;
+        this.h = 64;
   			this.y = Crafty.math.randomInt(0, Crafty.viewport.height);
   			this.z = 1;
+        this.origin("center");
         // this.gravityConst(0);
   
 				this.x = 0;
-				this._xspeed = Crafty.math.randomInt(3, 5);
+				this._xspeed = Crafty.math.randomInt(2, 4);
         this._yspeed = Crafty.math.randomInt(0, 2);
-        this._amp = Crafty.math.randomInt(3, 8);
+        this._amp = Crafty.math.randomInt(3, 6);
         this._freq = Crafty.math.randomInt(50, 80);
+        this._lastRotChange = Date.now();
+        this._rotDir = 1;
+        this._rotSpeed = Crafty.math.randomInt(1,7);
   
   			this.bind("EnterFrame", function() {
   				this.x += this._xspeed;
-          
           if (!this.hit) {
             this.y += this._amp * Math.cos(this.x/this._freq);
           }
+          
+          var rotAmt = this._rotSpeed;
+          var timeSinceLastRot = Date.now() - this._lastRotChange;
+          if (timeSinceLastRot > 1000) {
+            if (Crafty.math.randomInt(0,100) > 80) {
+              this._rotDir *= -1;
+              rotAmt *= 0.01;
+            }
+            // Only roll for rotation change 1 time per interval
+            this._lastRotChange = Date.now();
+          } else if (timeSinceLastRot < 100) {
+            rotAmt *= 0.01 * timeSinceLastRot;
+          }
+          this.rotation += this._rotDir * this._rotSpeed;
   
   				if (this._x > Crafty.viewport.width || (this._y > Crafty.viewport.height && this.hit)) {
   					this.destroy();
@@ -60,10 +71,10 @@ var Game = {
   
   			this.bind("MouseOver", function() {
   				this.hit = true;
-          this.color("brown");
           this._xspeed = 0;
           this.addComponent("Gravity").gravity();
           this.gravityConst(gravityConstHit);
+          this._rotSpeed *= -14/this._rotSpeed;
   				score += (index+1) * 10;
   				scoreEnt.text("Score: "+score);
   
@@ -75,7 +86,7 @@ var Game = {
   	Crafty.e().bind("EnterFrame", function(e) {
   		var sparsity = Crafty.math.randomInt(10, 50);
   		if(e.frame % sparsity == 0) {
-  			Crafty.e("2D, DOM, Leaf, Mouse");
+  			Crafty.e("2D, Canvas, Leaf, Mouse");
   		}
   	});
   }
